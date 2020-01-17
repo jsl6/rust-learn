e5ef9b334f75cfcc23835200b5c382ae711300bc
jsl6 <johnsonliang@aliyun.com>
2020-01-17 13:22:28 +0800

    feat: init project

---
+++ 01-hello-world/target/debug/deps/hello_world-84f984527bdfc20b.d
@@ -0,0 +1,5 @@
+/Users/zhaoxiongliang/work/github/jsl6/rust-learn/01-hello-world/target/debug/deps/hello_world-84f984527bdfc20b: src/main.rs
+
+/Users/zhaoxiongliang/work/github/jsl6/rust-learn/01-hello-world/target/debug/deps/hello_world-84f984527bdfc20b.d: src/main.rs
+
+src/main.rs:
---
+++ 01-hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/zhaoxiongliang/work/github/jsl6/rust-learn/01-hello-world/target/debug/hello-world: /Users/zhaoxiongliang/work/github/jsl6/rust-learn/01-hello-world/src/main.rs
